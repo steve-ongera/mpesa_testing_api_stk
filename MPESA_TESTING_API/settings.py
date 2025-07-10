8169e5a53f80435678b274339a835587aec517ca
steve <gadafisteve001@gmail.com>
2025-07-10 10:00:23 +0300

    updates

--- MPESA_TESTING_API/settings.py
+++ MPESA_TESTING_API/settings.py
@@ -102,9 +102,13 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # For development
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')    # For collectstatic in production
 
-STATIC_URL = 'static/'
+# Media files (Uploaded content)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
